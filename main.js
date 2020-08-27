6017f48988dce1533b29e457e40cb86fbdcfd4cf
PrabhuDakshin <prabhudakshin19@gmail.com>
2020-08-27 21:02:58 +0530

    building

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
+
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight);
 
@@ -7,20 +9,23 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 $('body').append(renderer.domElement);
 
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
+var geometry = new THREE.BoxGeometry(1, 2, 1);
 var edges = new THREE.EdgesGeometry(geometry);
 var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 'black'}));
-var material = new  THREE.MeshBasicMaterial({color: "red"});
+var texture = new THREE.TextureLoader().load('textures/buiding.jpg');
+// texture.setCrossOrigin('anonymous');
+var material = new  THREE.MeshBasicMaterial({map: texture});
+
 var cube = new THREE.Mesh(geometry, material);
 cube.add(line);
 scene.add(cube);
 
 cube.position.z = -3;
-cube.rotation.x = 10;
+cube.rotation.y = 5.5;
 //cube.rotation.y = -1;
 
 var animate = () => {
-    cube.rotation.x += .01;
+    // cube.rotation.x += .01;
     //cube.rotation.y += .01;
 
     renderer.render(scene, camera);
